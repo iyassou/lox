4944da42f86de4274ec8bf5c84c26e383fbf957e
Iyassou Shimels <s.iyassou@gmail.com>
2023-03-31 11:34:08 +0100

    jlox/src/.../lox/Interpreter.java: add resolver support

--- jlox/src/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,12 +1,15 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Interpreter implements    Expr.Visitor<Object>,
                                 Stmt.Visitor<Void> {
     final Environment globals = new Environment();
     private Environment environment = globals;
+    private final Map<Expr, Integer> locals = new HashMap<>();
 
     Interpreter() {
         globals.define("clock", new LoxCallable() {
@@ -42,6 +45,10 @@ class Interpreter implements    Expr.Visitor<Object>,
 
     private void execute(Stmt stmt) {
         stmt.accept(this);
+    }
+
+    void resolve(Expr expr, int depth) {
+        locals.put(expr, depth);
     }
 
     void executeBlock(List<Stmt> statements, Environment environment) {
@@ -132,7 +139,14 @@ class Interpreter implements    Expr.Visitor<Object>,
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = evaluate(expr.value);
-        environment.assign(expr.name, value);
+
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            environment.assignAt(distance, expr.name, value);
+        } else {
+            globals.assign(expr.name, value);
+        }
+        
         return value;
     }
 
@@ -216,6 +230,14 @@ class Interpreter implements    Expr.Visitor<Object>,
         }
         return function.call(this, arguments);
     }
+
+    @Override
+    public Object visitConditionalExpr(Expr.Conditional expr) {
+        if (isTruthy(evaluate(expr.predicate))) {
+            return evaluate(expr.iftrue);
+        }
+        return evaluate(expr.iffalse);
+    }
     
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
@@ -258,20 +280,16 @@ class Interpreter implements    Expr.Visitor<Object>,
     }
 
     @Override
-    public Object visitConditionalExpr(Expr.Conditional expr) {
-        if (isTruthy(evaluate(expr.predicate))) {
-            return evaluate(expr.iftrue);
-        }
-        return evaluate(expr.iffalse);
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return lookUpVariable(expr.name, expr);
     }
 
-    @Override
-    public Object visitVariableExpr(Expr.Variable expr) {
-        Object value = environment.get(expr.name);
-        if (value == null) {
-            throw new RuntimeError(expr.name, "Accessed uninitialised variable.");
+    private Object lookUpVariable(Token name, Expr expr) {
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            return environment.getAt(distance, name.lexeme);
         }
-        return value;
+        return globals.get(name);
     }
 
     private void checkNumberOperand(Token operator, Object operand) {

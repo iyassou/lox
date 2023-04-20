7233947685272b2cd4ca466b3902e298b0403a28
Iyassou Shimels <s.iyassou@gmail.com>
2023-04-20 16:25:10 +0100

    clox/chunk: implement run-length encoding compression

--- clox/chunk.c
+++ clox/chunk.c
@@ -9,12 +9,14 @@ void initChunk(Chunk* chunk) {
     chunk->capacity = 0;
     chunk->code = NULL;
     chunk->lines = NULL;
+    chunk->linesCount = 0;
+    chunk->linesCapacity = 0;
     initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-    FREE_ARRAY(int, chunk->lines, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->linesCapacity);
     freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
@@ -24,15 +26,48 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
-        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
-    chunk->lines[chunk->count] = line;
     chunk->count++;
+
+    // Update the corresponding line counter, if found.
+    for (int i = 0; i < chunk->linesCapacity; i += 2) {
+        if (chunk->lines[i+1] == line) {
+            // Increment the line counter.
+            chunk->lines[i]++;
+            return;
+        }
+    }
+    // Corresponding line counter wasn't found, add new entry,
+    // but first check if a resize is required.
+    if (chunk->linesCapacity < chunk->linesCount + 2) {
+        int oldCapacity = chunk->linesCapacity;
+        chunk->linesCapacity = GROW_CAPACITY(oldCapacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->linesCapacity);
+    }
+    chunk->lines[chunk->linesCount] = 1;        // line count
+    chunk->lines[chunk->linesCount + 1] = line; // line number
+    chunk->linesCount += 2;
 }
 
 int addConstant(Chunk* chunk, Value value) {
     writeValueArray(&chunk->constants, value);
     return chunk->constants.count - 1;
+}
+
+int getLine(Chunk* chunk, int index) {
+    // chunk->lines is an array of paired numbers, where the first
+    // number represents the count and the second number represents
+    // the line number.
+    int runningSum = 0;
+    for (int i = 0; i < chunk->linesCapacity; i += 2) {
+        if (runningSum <= index && index < runningSum + chunk->lines[i]) {
+            // if the index is sandwiched between the running total and
+            // the next few lines, then we've found the corresponding line
+            return chunk->lines[i+1];
+        }
+        runningSum += chunk->lines[i];
+    }
+    exit(1);
 }
--- clox/chunk.h
+++ clox/chunk.h
@@ -14,6 +14,8 @@ typedef struct {
     int capacity;
     uint8_t* code;
     int* lines;
+    int linesCount;
+    int linesCapacity;
     ValueArray constants;
 } Chunk;
 
@@ -21,5 +23,6 @@ void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
+int getLine(Chunk* chunk, int index);
 
 #endif

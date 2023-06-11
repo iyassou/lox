90cf21c37beed9004c9e5a7aa23ba695f730772e
Iyassou Shimels <s.iyassou@gmail.com>
2023-06-11 11:55:37 +0100

    clox/object: support intering and hashing strings

--- clox/object.c
+++ clox/object.c
@@ -3,6 +3,7 @@
 
 #include "memory.h"
 #include "object.h"
+#include "table.h"
 #include "value.h"
 #include "vm.h"
 
@@ -18,21 +19,40 @@ static Obj* allocateObject(size_t size, ObjType type) {
     return object;
 }
 
-static ObjString* allocateString(char* chars, int length) {
+static ObjString* allocateString(char* chars, int length, uint32_t hash) {
     size_t size = sizeof(ObjString) + (length + 1) * sizeof(char);
     ObjString* string = (ObjString*)allocateObject(size, OBJ_STRING);
     string->length = length;
+    string->hash = hash;
     memcpy(string->chars, chars, length);
     string->chars[length] = '\0';
+    tableSet(&vm.strings, string, NIL_VAL);
     return string;
 }
 
+static uint32_t hashString(const char* key, int length) {
+    uint32_t hash = 2166136261u;
+    for (int i = 0; i < length; i++) {
+        hash ^= (uint8_t)key[i];
+        hash *= 16777619;
+    }
+    return hash;
+}
+
 ObjString* takeString(char* chars, int length) {
-    return allocateString(chars, length);
+    uint32_t hash = hashString(chars, length);
+    ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
+    if (interned != NULL) return interned;
+
+    return allocateString(chars, length, hash);
 }
 
 ObjString* copyString(const char* chars, int length) {
-    return allocateString((char*)chars, length);
+    uint32_t hash = hashString(chars, length);
+    ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
+    if (interned != NULL) return interned;
+
+    return allocateString((char*)chars, length, hash);
 }
 
 void printObject(Value value) {

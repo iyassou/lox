8b471c500c1ee45ba5110ecc06f51d3522c6461f
Iyassou Shimels <s.iyassou@gmail.com>
2023-06-16 16:02:05 +0100

    clox/vm.c: string concatenation needn't free its buffer

--- clox/vm.c
+++ clox/vm.c
@@ -65,12 +65,12 @@ static void concatenate() {
     ObjString* a = AS_STRING(pop());
 
     int length = a->length + b->length;
-    char* chars = ALLOCATE(char, length);
+    char* chars = ALLOCATE(char, length + 1);
     memcpy(chars, a->chars, a->length);
     memcpy(chars + a->length, b->chars, b->length);
+    chars[length] = '\0';
 
     ObjString* result = takeString(chars, length);
-    reallocate(chars, length, 0);
     push(OBJ_VAL(result));
 }
 

538d84dfec2daf24de709e2bad4ccf17c7c0826b
Iyassou Shimels <s.iyassou@gmail.com>
2023-06-16 21:04:58 +0100

    clox/main.c: memory leaks make me sick

--- clox/main.c
+++ clox/main.c
@@ -53,8 +53,14 @@ static void runFile(const char* path) {
     InterpretResult result = interpret(source);
     free(source);
 
-    if (result == INTERPRET_COMPILE_ERROR) exit(65);
-    if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+    if (result == INTERPRET_COMPILE_ERROR) {
+        freeVM();
+        exit(65);
+    }
+    if (result == INTERPRET_RUNTIME_ERROR) {
+        freeVM();
+        exit(70);
+    }
 }
 
 int main(int argc, const char* argv[]) {
